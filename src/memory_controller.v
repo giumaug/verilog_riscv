c254c28400122565bd4469abb9b160cbe4c66016
peppe <peppe@localhost.localdomain>
2022-03-13 17:40:42 +0100

    commit

---
+++ src/memory_controller.v
@@ -0,0 +1,14 @@
+module memory_controller(input[31:0] address_1,
+                         input[31:0] i_val_1,
+                         input op_type_1,
+					     input[31:0] address_2,
+                         input[31:0] i_val_2,
+                         input op_type_2,
+                         output[31:0] o_val_1,
+						 output[31:0] o_val_2);
+
+	always @(...)
+    begin
+
+	end
+endmodule     
--- src/register_file.v
+++ src/register_file.v
@@ -1,24 +1,35 @@
-module register_file(input[4:0] i_r_reg_num_1,
+module register_file(input rst,
+					 input[4:0] i_r_reg_num_1,
                      input[4:0] i_r_reg_num_2,
                      input[4:0] i_w_reg_num,
                      input[31:0] i_w_val,
+		             input op,
                      output reg[31:0] r_reg_1,
                      output reg[31:0] r_reg_2);
-                     
-	reg[31:0] registers[3:0];
+       
+	integer i;              
+	reg[31:0] registers[4:0];
+
+	always@(rst) begin
+		if (rst == 1) begin
+			for (i = 0; i < 32; i++) begin
+				registers[i] <= 0;
+			end
+		end
+	end
 	
-//	assign r_reg_1 = registers[i_r_reg_num_1]; 
-//	assign r_reg_2 = registers[i_r_reg_num_2]; 
 	 
-	always @(registers[i_r_reg_num_1] or registers[i_r_reg_num_2])
+    always @(op)
     begin
-    	r_reg_1 <= registers[i_r_reg_num_1]; 
-		r_reg_2 <= registers[i_r_reg_num_2]; 
-    end
-    
-    always @(i_w_reg_num or i_w_val)
-    begin
-    	registers[i_w_reg_num] <= i_w_val;
+		if (op == 0) begin
+			$display("000 %d", op);
+    		r_reg_1 <= registers[i_r_reg_num_1]; 
+			r_reg_2 <= registers[i_r_reg_num_2];
+		end
+        if (op == 1) begin
+			$display("111 %d",op);
+    		registers[i_w_reg_num] <= i_w_val;
+		end
     end
 endmodule
              
