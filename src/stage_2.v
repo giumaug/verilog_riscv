9803467655bde27625beccf85079cdef14c7b8ad
giuseppe maugeri <g.maugeri@octorate.com>
2022-04-04 18:01:12 +0200

    commit

--- src/stage_2.v
+++ src/stage_2.v
@@ -15,6 +15,8 @@ module stage_2(input rst,
 	wire[4:0] reg_num_2 = if_id_instr[24:20];
 	wire[4:0] w_reg_num = if_id_instr[24:20];
 	wire[6:0] opcode = if_id_instr[6:0]; 
+	wire[6:0] func_7 = if_id_instr[31:25];
+	wire[2:0] func_3 = if_id_instr[14:12];
 	reg reg_file_op <= 0;
     reg[31:0] reg_1;
 	reg[31:0] reg_2;
@@ -46,18 +48,75 @@ module stage_2(input rst,
 					if (imm_20[0] == 0) b_pc <= if_id_pc + {imm20, 1'b0};
 					else b_pc <=  if_id_pc - ( ~ ({imm20, 1'b0} - 1));
 					b_taken <= 1;
+					$display("instruction JAL");
 			    end
 				`JALR: begin
-					if (imm_12[11] == 0) b_pc <= partire da qui e verificare che la pipeline funziona sul reg file!!!!!!!!!!!!!!!!!!!!!!!
+					if (imm_12[11] == 0) b_pc <= (reg_1 + imm_12_i(word)) & 4094;
+					else b_pc <= (reg_1 - ( ~ (imm_12_i(word) - 1))) & 4094;
+					b_taken <= 1;
+					$display("instruction JALR");
 				end
+				`BRANCH: begin
+				    case (func_3)
+				        `BEQ: begin
+				            if (reg_1 == reg_2) b_taken <= 1;
+				            else b_taken <= 0;
+				            $display("instruction BEQ")
+				        end
+                        `BNE: begin
+                             if (reg_1 != reg_2) b_taken <= 1;
+                             else b_taken <= 0;
+                             $display("instruction BNE");
+                        end
+                        `BLT: begin
+						    if (reg_1 < reg_2) b_taken <= 1;
+							else b_taken <= 0;
+							$display("instruction BLT");	
+						end
+						`BLTU: begin
+						    if (abs(reg_1) < abs(reg_2)) b_taken <= 1;
+					        else _bTaken <= 0;
+							$display("instruction BLTU");	
+						end
 
-
-				imm12 = rImm12I(word);
-			     		if (imm12[11] == 0) _bPc = (rs1 + extend(imm12)) & 4094;
-			     		else _bPc = (rs1 - zeroExtend( ~ (imm12 - 1))) & 4094;
-			        	_bTaken = True;
-			        	`DISPLAY("instruction JALR")
-
+		
+`BRANCH: begin
+						imm12 = rImm12B(word);
+			     		_bPc = ifId.rPc() + signExtend(imm12 << 1);
+			     		_rdNum = 0;
+						case (func3) 
+							`BEQ: begin
+								if (rs1 == rs2) _bTaken = True;
+								else _bTaken = False;
+								`DISPLAY("instruction JALR")
+							end	
+							`BNE: begin
+								if (rs1 != rs2) _bTaken = True;
+								else _bTaken = False;
+								`DISPLAY("instruction BNE")
+							end
+							`BLT: begin
+								if (rs1 < rs2) _bTaken = True;
+								else _bTaken = False;
+								`DISPLAY("instruction BLT")
+							end
+							`BLTU: begin
+								if (abs(rs1) < abs(rs2)) _bTaken = True;
+								else _bTaken = False;
+								`DISPLAY("instruction BLTU")
+							end
+							`BGE: begin
+								if (rs1 > rs2) _bTaken = True;
+								else _bTaken = False;
+							end
+							`BGEU: begin
+								if (abs(rs1) > abs(rs2)) _bTaken = True;
+								else _bTaken = False;
+								`DISPLAY("instruction BGEU")
+							end
+							//default: bTaken <= False;
+						endcase
+					end
 
 			endcase
 			
