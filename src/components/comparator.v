28e67e2451a0aee685a07251317ec1f527488065
peppe <giumaug@gmail.com>
2022-10-31 19:52:06 +0100

    commitììììì

--- src/components/comparator.v
+++ src/components/comparator.v
@@ -1,35 +1,19 @@
-module comparator(input sign i_sign,
-               input[31:0] a,
-               input[31:0] b,
-			   output reg[31:0] gt,
-			   output reg[31:0] eq,
-			   output reg[31:0] lt);
-	
-	wire[33:0] n;
-	wire cn;
-	wire z;
-	
-	assign cn = ~a + b;
-	assign c = cn[33];
-	assign n = cn[32:0];
-	assign z = and(~n);
-	
-	assign gt = ~a | z;
-	assign eq = z;
-	assign lt = c;
-	
-endmodule	
+module comparator (input[31:0] i_in_1,
+				   input [31:0] i_in_2,
+				   input sign,
+				   output gt,
+                   output lt,
+                   output eq); 
 
------------
-module comparator (input[7:0]  In1,
-       input [7:0] In2,
-       output Gt,
-       output Lt,
-       output Eq); 
-
-    always @(*) begin 
-     Gt <= ( In1 > In2 )? 1'b1 : 1'b0; 
-     Lt <= ( In1 < In2 )? 1'b1 : 1'b0; 
-     Eq <= ( In1 == In2)? 1'b1 : 1'b0; 
+	always @(*) begin
+		if (sign == 1) begin
+			gt <= ( $signed(i_in_1) > $signed(i_in_2)) ? 1'b1 : 1'b0; 
+			lt <= ( $signed(i_in_1) < $signed(i_in_2)) ? 1'b1 : 1'b0; 
+			eq <= ( $signed(i_in_1) == $signed(i_in_2)) ? 1'b1 : 1'b0;
+		end else begin
+			gt <= ( i_in_1 > i_in_2 ) ? 1'b1 : 1'b0; 
+			lt <= ( i_in_1 < i_in_2 ) ? 1'b1 : 1'b0; 
+			eq <= ( i_in_1 == i_in_2) ? 1'b1 : 1'b0;
+		end 
     end 
-    endmodule
+endmodule
